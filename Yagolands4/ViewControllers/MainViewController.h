82cfc0d0026c76649dab4e01e1e0aa852b64e6a9
Simone <sensorario@gmail.com>
2013-01-14 05:10:10 +0100

    Rimossi i commenti inutili

--- Yagolands4/ViewControllers/MainViewController.h
+++ Yagolands4/ViewControllers/MainViewController.h
@@ -1,11 +1,3 @@
-//
-//  MainViewController.h
-//  Yagolands4
-//
-//  Created by Simone Gentili on 12/01/13.
-//  Copyright (c) 2013 SENSORARIO. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "Y4Coordinata.h"
 
--- Yagolands4/Y4AppDelegate.h
+++ Yagolands4/Y4AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  Y4AppDelegate.h
-//  Yagolands4
-//
-//  Created by Simone Gentili on 23/12/12.
-//  Copyright (c) 2012 SENSORARIO. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface Y4AppDelegate : UIResponder <UIApplicationDelegate>
